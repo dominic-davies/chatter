04927f15656bdad7ab6581f4e868bd417fe3657f
Dominic Davies <domdaviesit@gmail.com>
2019-03-10 21:32:03 +0000

    fix issue with chatbox not showing messages typed

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,16 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user!
+
   def create
+    # First create the message and assign the user.
+    @message = Message.new(message_params)
+    @message.user = current_user
+    @message.save
+  end
+
+
+  private
+  def message_params
+    params.require(:message).permit(:body)
   end
 end
